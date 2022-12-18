6899ac515b4f7acd7a2f8fff194f871e67b0456e
Niki Wix Skaarup <niki@skaarup.dev>
2022-12-18 02:06:58 +0100

    types

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 import type { UserConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
+import { isoImport } from 'vite-plugin-iso-import';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), imagetools()]
+	plugins: [sveltekit(), imagetools(), isoImport()],
+	resolve: {
+		alias: {
+			$types: path.resolve(__dirname, './types')
+		}
+	}
 };
 
 export default config;
